750e426cbee7ce676e42d012d4f3515de79c7c53
Rafael Calmon Gonzaga Santos <rafacalmon27@gmail.com>
2026-04-23 16:22:25 -0300

    Adicionando Headers

--- src/infrastructure/services/FilazeroApiService.ts
+++ src/infrastructure/services/FilazeroApiService.ts
@@ -6,8 +6,27 @@ import type {AvailableDate} from "../../domain/models/availableDate.js"
 export class FilazeroApiService {
   private BASE_URL = "https://api.staging.filazero.net";
 
+  private getHeaders() {
+  return {
+    Accept: "application/json, text/plain, */*",
+    Origin: "https://app.filazero.net",
+    Referer: "https://app.filazero.net/",
+    "User-Agent": "MCP-Server-FilaZero/1.0",
+    DNT: "1",
+  };
+  }
+
+  // Para requisições POST
+  private getWriteHeaders(token?: string) {
+  return {
+    ...this.getHeaders(),
+    "Content-Type": "application/json;charset=UTF-8",
+    ...(token && { Authorization: `Bearer ${token}` }),
+  };
+  }
+
   async getCompanies(): Promise<Company[]> {
-    const response = await fetch(`${this.BASE_URL}/api/companies`);
+    const response = await fetch(`${this.BASE_URL}/api/companies`, {headers: this.getHeaders()});
 
     if (!response.ok) {
       throw new Error(`Erro HTTP ao buscar empresas: ${response.status}`);
@@ -20,7 +39,7 @@ export class FilazeroApiService {
 
   async getCompanyServices(slug: string): Promise<Service[]> {
     try {
-        const response = await fetch(`${this.BASE_URL}/api/companies/${slug}/services`)
+        const response = await fetch(`${this.BASE_URL}/api/companies/${slug}/services`, {headers: this.getHeaders()})
 
         if (!response.ok) {
           throw new Error(`Erro HTTP ao buscar serviços da empresa com slug ${slug}: ${response.status}`);
@@ -38,7 +57,7 @@ export class FilazeroApiService {
 
   async getAvailableDates(slug: string, serviceId: number): Promise<AvailableDate[]> {
     try {
-      const response = await fetch(`${this.BASE_URL}/v2/scheduling/self-service/providers/${slug}/services/${serviceId}/available-session-days`);
+      const response = await fetch(`${this.BASE_URL}/v2/scheduling/self-service/providers/${slug}/services/${serviceId}/available-session-days`, {headers: this.getHeaders()});
 
       if (!response.ok) {
         throw new Error(`Erro HTTP: ${response.status}`)
